277f283f8c6de21edc91f426525c4b78bec193f8
bernix01 <gbernal096@gmail.com>
2017-06-10 16:21:38 -0500

    run on port 80

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.use('/',express.static(__dirname + '/public/app'));
 // });
 
 app.listen(3000, function() {
-  console.log('Node app is running on port', 3000);
+  console.log('Node app is running on port', 80);
 });
 
 
