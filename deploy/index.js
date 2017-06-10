125e7546a071d46872a1a0a3434a5f72fb1fb172
bernix01 <gbernal096@gmail.com>
2017-06-10 17:03:24 -0500

    deployment files

--- deploy/index.js
+++ deploy/index.js
@@ -4,9 +4,9 @@ var express = require("express");
 var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use('/', express.static(__dirname + '/public/app'));
-app.get('/', function (req, res) {
-    res.send('Hello World');
-});
+// app.get('/', function(req, res){
+//   res.send('Hello World');
+// });
 app.listen(process.env.PORT || 80, function () {
     console.log('Node app is running on port', process.env.PORT || 80);
 });
