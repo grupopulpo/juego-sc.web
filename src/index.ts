5d8e94cb79500e81b9120112ac82bb5db333d5a3
bernix01 <gbernal096@gmail.com>
2017-06-10 16:57:20 -0500

    test root

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,9 @@ app.use(express.static(__dirname + '/public'));
 
 app.use('/',express.static(__dirname + '/public/app'));
 
-// app.get('/', function(req, res){
-//   res.send('Hello World');
-// });
+app.get('/', function(req, res){
+  res.send('Hello World');
+});
 
 app.listen(process.env.PORT || 80, function() {
   console.log('Node app is running on port', process.env.PORT || 80);
