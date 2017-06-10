1e9f3a2bdb61756926a8268c6a3090101a800dc2
Bryan <bsteven1993@gmail.com>
2017-06-10 15:07:15 -0500

    make it better

--- src/public/app/game/states/preloader.ts
+++ src/public/app/game/states/preloader.ts
@@ -41,7 +41,7 @@ module GameApp.States {
          this.game.load.json('level3', assets+'levels/Level_3.json');
          this.game.load.json('level4', assets+'levels/Level_4.json');
          this.game.load.json('level5', assets+'levels/Level_5.json');
-         this.game.load.json('level6', assets+'levels/Level_6.json');
+         this.game.load.json('level6', assets+'levels/Level_6.json'); //sterben
 
          
          this.game.load.audio('swapSound', assets+'sounds/Chomp.wav');
